c503b6fa3e94d080c14df4b7cfc1cce8e2174671
szpn <szopen1290@gmail.com>
2019-10-26 00:11:21 +0200

    ?

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
+var createError = require('http-errors');
 var express = require('express');
-var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
-// Routes
-app.get('/', function(req, res) {
-  res.render("index")
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+//parse requests
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Listen
-var port = process.env.PORT || 3000;
-app.listen(port);
-console.log('Listening on localhost:'+ port);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, 'public')));
+  // render the error page
+  res.status(err.status || 500);  
+  res.render('error.pug');
+});
 
 module.exports = app;
